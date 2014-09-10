f0f57640fd17340079f957d10ca8d75e3bf65a2d
Caleb Reister <calebreister@gmail.com>
2014-09-09 20:47:02 -0700

    Added pointer section.

--- html/cppmem.js
+++ html/cppmem.js
@@ -3,7 +3,13 @@ var cppmem =
     [ "The Stack", "cppmem.html#cppmemstack", null ],
     [ "The Heap", "cppmem.html#cppmemheap", null ],
     [ "Binary & Hexadecimal", "cppmembinhex.html", null ],
-    [ "Pointers", "cppmemptr.html", null ],
+    [ "Pointer", "cppmemptr.html", [
+      [ "Scope Issues", "cppmemptr.html#cppmemptrscope", null ],
+      [ "Arrays", "cppmemptr.html#cppmemptrarray", null ],
+      [ "Pointer Arithmetic", "cppmemptr.html#cppmemptrmath", null ],
+      [ "Pointers to Pointers", "cppmemptr.html#cppmemptrptr", null ],
+      [ "The void*", "cppmemptr.html#cppmemptrvoid", null ]
+    ] ],
     [ "Dynamic Arrays", "cppmemarr.html", null ],
     [ "Dynamic Objects", "cppmemobj.html", null ]
 ];
--- html/search/all_a.js
+++ html/search/all_a.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['program_20flow',['Program Flow',['../cppflow.html',1,'cpp']]],
   ['program_20layout',['Program Layout',['../cpplayout.html',1,'cpp']]],
-  ['pointers',['Pointers',['../cppmemptr.html',1,'cppmem']]],
+  ['pointer',['Pointer',['../cppmemptr.html',1,'cppmem']]],
   ['pointers_2edox',['pointers.dox',['../pointers_8dox.html',1,'']]]
 ];
